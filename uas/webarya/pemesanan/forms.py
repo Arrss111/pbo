9fd96130cf4927d9d917d97563f2577ba993b08f
Daniswara Aryaguna <a27ya.7@gmail.com>
2025-02-02 18:19:20 +0700

    form lebih baik

--- uas/webarya/pemesanan/forms.py
+++ uas/webarya/pemesanan/forms.py
@@ -1,12 +1,13 @@
 from django import forms
-from .models import order
+from .models import order, karyaSeni
 
 
 class orderform(forms.ModelForm):
     full_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Full Name'}), required=True)
     email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'email'}), required=True)
     deskripsi = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'deskripsi'}), required=True)
-    Nomor_HP = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nomor_HP'}), required=False)
+    Nomor_HP = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nomor_HP'}), required=True)
+    product = forms.ModelChoiceField(label="", queryset=karyaSeni.objects.all(), widget=forms.Select(attrs={'class':'form-control', 'placeholder':'Karya_seni'}), required=True)
 
 
 
--- uas/webarya/pemesanan/models.py
+++ uas/webarya/pemesanan/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from aryaapp.models import karyaSeni
 
+
 class order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     full_name = models.CharField(max_length=100)
@@ -10,21 +11,12 @@ class order(models.Model):
     Nomor_HP = models.CharField(max_length=16, null=True, blank=True)
     amount_paid = models.DecimalField(default=0, max_digits=6, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
-    
+    product = models.ForeignKey(karyaSeni, on_delete=models.CASCADE, null=True)    
 
     def __str__(self):
         return f'order - {str(self.id)}'
     
-class orderItem(models.Model):
-    order = models.ForeignKey(order, on_delete=models.CASCADE, null=True)
-    product = models.ForeignKey(karyaSeni, on_delete=models.CASCADE, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
-    quantity = models.PositiveBigIntegerField(default=1)
-    harga = models.DecimalField(default=0, max_digits=6, decimal_places=2)
-
-    def __str__(self):
-        return f'orderItem - {str(self.id)}'
     
 class pemesanan(models.Model):
     proses = models.CharField(max_length=100)
@@ -32,3 +24,18 @@ class pemesanan(models.Model):
     def __str__(self):
         return self.proses
     
+    class Meta:
+        verbose_name_plural = 'pemesanan'
+
+
+class orderItem(models.Model):
+    order = models.ForeignKey(order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(karyaSeni, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    pesan = models.ForeignKey(pemesanan,default=1, on_delete=models.CASCADE, null=True)
+    
+    quantity = models.PositiveBigIntegerField(default=1)
+    harga = models.DecimalField(default=0, max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f'orderItem - {str(self.id)}'
